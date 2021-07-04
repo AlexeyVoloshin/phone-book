756f8809daab11b6a0970d5b42f9fe6d7e480d20
Alexey Voloshin <voloshinalex1100@gmail.com>
2021-07-04 10:57:48 +0300

    Add ESlint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
+
 import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={s.App}>
+      <header className={s.App_header}>
+        <img src={logo} className={s.App_logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={cn(s.App_link, s.color_text)}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
