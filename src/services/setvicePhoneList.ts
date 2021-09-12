c37caba6c9efd67620666fac293004eeba0e4090
Max Bondarenko <max.bondarenk@gmail.com>
2021-09-12 14:07:22 +0300

    add fix

--- src/services/setvicePhoneList.ts
+++ src/services/setvicePhoneList.ts
@@ -1,26 +1,21 @@
-import { ITelephoneBook } from "../model/phone";
+import { ITelephoneBook } from '../model/phone';
 
 class PhoneBookService {
+  static getAllPhones = () => {
+    /**
+     * TODO get data from the local store check if the link to the link is loaded from the file to the side
+     */
+    const phoneList = localStorage.getItem('phoneList');
 
- static getAllPhones = () => {
-		/**
-		 * TODO получить данные из локал стора
-		 */
-		const phoneList = localStorage.getItem("phoneList")
+    return phoneList !== null ? JSON.parse(phoneList) : [];
+  };
 
-		return phoneList !== null 
-			? JSON.parse(phoneList) : []
-	}
-
-static addPhone = async (data: ITelephoneBook[]) => {
-		/**
-		 * TODO добавить данные в локал стор
-		 */
-		localStorage.setItem('phoneList', JSON.stringify(data));
-
-	}
+  static addPhone = async (data: ITelephoneBook[]) => {
+    /**
+     * TODO add data to local store
+     */
+    localStorage.setItem('phoneList', JSON.stringify(data));
+  };
 }
-
-
 
 export default PhoneBookService;
